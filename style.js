bd682d4a7d83a3d220020eea99bb7045318de053
Hoang Duy <76449669+ps18293@users.noreply.github.com>
2021-01-20 03:06:49 +0700

    Update file

--- style.js
+++ style.js
@@ -1,27 +1,26 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
-        
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// Responsive Top Nav
 function menuFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-      x.className += " responsive";
-  } else {
-      x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
+// Back to top
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var showAfter = 100;
+        if ($(this).scrollTop() > showAfter) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
